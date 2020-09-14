d4effa346e8db6b9ed5a2d1e26a0ae93e6bee343
fechad <fedwin.chatelier@polymtl.ca>
2020-09-14 18:13:04 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,7 +10,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return (angle_degs+ (an/180
+    return (angle_degs+ (angle_mins*360//180
 
 
 def to_degrees(angle_rads: float) -> tuple:
